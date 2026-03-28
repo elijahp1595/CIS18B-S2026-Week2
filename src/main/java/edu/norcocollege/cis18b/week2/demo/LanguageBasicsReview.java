e3e89c6c48b1df4d33aa12e64acf4247d795319e
elijahp1595 <pina.elijah@gmail.com>
2026-03-28 02:42:37 +0000

    I fixed a type mismatch when writing my code. I added a rightTriangleCalculate method. Additionally, I implemented a static method call, which is Math.sqrt().

--- src/main/java/edu/norcocollege/cis18b/week2/demo/LanguageBasicsReview.java
+++ src/main/java/edu/norcocollege/cis18b/week2/demo/LanguageBasicsReview.java
@@ -16,26 +16,27 @@ public class LanguageBasicsReview {
     public void demonstrate() {
         // TODO: Implement demonstration of basic Java features
         
-
-
-
         // NOTE: Remove the exception below when you've implemented your code
         // throw new UnsupportedOperationException("Not implemented yet");
     }
 
-    class Triangle {
-        private int sideA;
-        private int sideB;
-        private int sideC;
+    class rightTriangle {
+        private double sideA;
+        private double sideB;
+        private double sideC;
 
-        public Triangle() {
-            this(0,0,0);
+        public rightTriangle() {
+            this(0,0);
         }
 
-        public Triangle(int sA, int sB, int sC) {
+        public rightTriangle(double sA, double sB) {
             this.sideA = sA;
             this.sideB = sB;
-            this.sideC = sC;
+        }
+
+        public double rightTriangleCalculate() {
+            sideC = Math.sqrt((sideA * sideA)+(sideB * sideB));
+            return sideC;
         }
     }
 
