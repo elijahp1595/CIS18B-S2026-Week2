f518150382f9132e3bffc6301153b7d48a0b4a11
elijahp1595 <pina.elijah@gmail.com>
2026-03-29 00:21:37 +0000

    I have completed the Menu class that displays a menu for the two classes. The user is able to select which calculation they can perform based on their selection using a switch case.

--- src/main/java/edu/norcocollege/cis18b/week2/demo/LanguageBasicsReview.java
+++ src/main/java/edu/norcocollege/cis18b/week2/demo/LanguageBasicsReview.java
@@ -1,4 +1,5 @@
 package edu.norcocollege.cis18b.week2.demo;
+import java.util.Scanner;
 
 /**
  * This class is a review of basic Java language features. It’s not
@@ -15,9 +16,50 @@ public class LanguageBasicsReview {
 
     public void demonstrate() {
         // TODO: Implement demonstration of basic Java features
-        
+        Menu menu = new Menu();
+        menu.start();
         // NOTE: Remove the exception below when you've implemented your code
         // throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    class Menu {
+        Square option2 = new Square();
+        Scanner input = new Scanner(System.in);
+        public void start(){
+            int option;
+            do {
+                System.out.println("\n\nPlease select an option:");
+                System.out.println("1. Calculate Side C of Triangle.");
+                System.out.println("2. Calculate Rectangular Area.");
+                System.out.println("3. Exit Program.");
+                option = input.nextInt();
+                switch (option) {
+                    case 1:
+                        System.out.print("Please enter side A: ");
+                        double A = input.nextDouble();
+                        System.out.print("Please enter side B: ");
+                        double B = input.nextDouble();
+                        rightTriangle option1 = new rightTriangle(A,B);
+                        double C = option1.rightTriangleCalculate();
+                        System.out.print("Side C = " + C);
+                        break;
+                    case 2:
+                        System.out.print("Please enter side A: ");
+                        double a = input.nextDouble();
+                        System.out.print("Please enter side B: ");
+                        double b = input.nextDouble();
+                        Square option2 = new Square(a,b);
+                        double area = option2.squareAreaCalculate();
+                        System.out.print("Area = " + area);
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        System.out.println("Invalid Selection, please choose again.");
+                        break;
+                }
+            } while (option != 3);
+        }
     }
 
     class rightTriangle {
